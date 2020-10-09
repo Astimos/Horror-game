354459f6e6c93d27b36577a012aa1855c6cc9f6f
Astimos <astimos5967@gmail.com>
2020-10-09 20:32:30 +0200

    +project-transfer-github
    Let the fun begin

---
+++ Game v.01/Assets/Flashlight/Flashlight.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    private bool flashlightEnabled;
+    public GameObject flashlight;
+    public GameObject lightObj;
+    private float currentEnergy;
+
+    public void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        //equip
+        if (Input.GetKeyDown(KeyCode.F)) 
+        {
+            flashlightEnabled =! flashlightEnabled;
+        }
+
+        if (flashlightEnabled)
+        {
+            flashlight.SetActive(true);
+        }
+        else 
+        {
+            flashlight.SetActive(false);
+        }
+    }
+}
