5b7794739a5cd89cf367f7ad7db03798d8947312
Astimos <astimos5967@gmail.com>
2020-10-10 15:32:33 +0200

    +battery-system
    Battery system that allows your flashlight to work when you have charge

--- Game v.01/Assets/Flashlight/Flashlight.cs
+++ Game v.01/Assets/Flashlight/Flashlight.cs
@@ -6,27 +6,88 @@ public class Flashlight : MonoBehaviour
     public GameObject flashlight;
     public GameObject lightObj;
     private float currentEnergy;
+    public float maxEnergy;
+
+    private float batteries;
+    private GameObject batteryPickedUp;
+    private float usedEnergy;
+
 
     public void Start()
     {
-        
+        currentEnergy = maxEnergy;
+        maxEnergy = 50 * batteries;
     }
 
-    public void Update()
+    public void FixedUpdate()
     {
-        //equip
+
+        maxEnergy = 50 * batteries;
+        currentEnergy = maxEnergy;
+
+
+        //Turn on|off by pressing F
+
         if (Input.GetKeyDown(KeyCode.F)) 
         {
             flashlightEnabled =! flashlightEnabled;
         }
 
+        /* Enabling flashling
+
+        if (flashlightEnabled)
+       {
+             flashlight.SetActive(true);
+      }
+        else 
+       {
+          flashlight.SetActive(false);
+        }
+
+        */
+
+
         if (flashlightEnabled)
         {
             flashlight.SetActive(true);
+
+
+            currentEnergy -= 0.5f * Time.deltaTime;
+            if (currentEnergy <= 0) 
+            {
+                lightObj.SetActive(false);
+                batteries = 0;
+            }
+            if (currentEnergy > 0) 
+            {
+                lightObj.SetActive(true);
+                currentEnergy -= 0.5f * Time.deltaTime;
+                usedEnergy += 10f * Time.deltaTime;
+            }
+
+            if (usedEnergy >= 50) 
+            {
+                batteries -= 1;
+                usedEnergy = 0;
+            }
         }
+
         else 
         {
-            flashlight.SetActive(false);
+             flashlight.SetActive(false);
+        }
+
+        print("Batteires: " + batteries);
+        print("usedEnergy: " + usedEnergy);
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Battery") {
+            batteryPickedUp = other.gameObject;
+            batteries += 1;
+            Destroy(batteryPickedUp);
         }
     }
 }
